b7253610674dc547dfb21141ed518bf4f9f2b388
Tanishq <tanishquppal220@gmail.com>
2022-12-03 11:24:06 +0530

    Q1 solved

--- Practice/diffrent charaters.py
+++ Practice/diffrent charaters.py
@@ -19,26 +19,27 @@
 # print(b)
 
 #!Important program It is used to count the number of words in a string by using count() function
-a=input("Enter a String: ")
-a=a.upper()
-d={'Total Alphabets':0,'Total Digits':0,'Total Special Characters':0}
-for i in range(0,122):
-    b=chr(i)
-    if b in a:
-        d[b]=a.count(b)
-for i in d:
-    if i.isalpha():
-        d['Total Alphabets']+=d[i]
-    elif i.isdigit():
-        d['Total Digits']+=d[i]
-    else:
-        d['Total Special Characters']+=d[i]
-for i in d:
-    print(i,"=",d[i],end=",")
-
 # a=input("Enter a String: ")
-# for i in range(0,len(a)):
-#     print(a[i   :i+2])
+# a=a.upper()
+# d={'Total Alphabets':0,'Total Digits':0,'Total Special Characters':0}
+# for i in range(0,122):
+#     b=chr(i)
+#     if b in a:
+#         d[b]=a.count(b)
+# for i in d:
+#     if i.isalpha():
+#         d['Total Alphabets']+=d[i]
+#     elif i.isdigit():
+#         d['Total Digits']+=d[i]
+#     else:
+#         d['Total Special Characters']+=d[i]
+# for i in d:
+#     print(i,"=",d[i],end=",")
+
+a=input("Enter a String: ")
+a.replace(" ",".")
+for i in range(0,len(a),2):
+    print(a[i:i+2])
     
 # a="Hello World"
 # print(a[0:len(a)+6], end=" ")
