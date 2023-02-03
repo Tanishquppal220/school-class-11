11d4bbc3610b1cbaa89f0f0ba3e81006408ef0ed
tanishqschool <118065548+tanishqschool@users.noreply.github.com>
2023-02-03 07:16:56 +0530

    Changes

--- all Paractical/5(3).py
+++ all Paractical/5(3).py
@@ -3,6 +3,9 @@ n = int(input("Enter the value of n: "))
 sum=0
 for i in range(1, n + 1):
     term = x ** i / i
-    sum +=term
+    if i%2==0:
+        sum -=term
+    else:
+        sum += term
 print("Sum =", sum)
 
